c7db6cd4de57561a039486d001c0187f9b0dcdf8
GrayGalaxy <ag.avirup1997@gmail.com>
2021-10-20 18:50:41 +0530

    new update

--- src/inject.js
+++ src/inject.js
@@ -10,27 +10,43 @@ const _done = (icon, item, msg, err = false) => {
 const add_song_download_btn = () => {
 	// Add button in each song
 	$('li').find('figcaption').find('a.u-color-js-gray').each(function () {
-		if ($(this).parents('article.o-snippet').find('.single_download_btn').length !== 0) return
-		const Button = $('<div class="o-snippet__item single_download_btn"><span class="u-link"><i class="o-icon--large o-icon-download"></i></span></div>')
-		const icon = Button.find('span').find('i.o-icon--large')
+		const that = $(this),
+			article = that.parents('article.o-snippet'),
+			cls = 'single_download_btn'
+		if (article.find(`.${cls}`).length !== 0) return
+
 		try { var token = this.href.match(/.*\/(.*)/)[1] } catch (e) { return }
+
+		const btn = $(`
+		<div class="o-snippet__item single_download_btn s_prg" data-song=${token}>
+			<span class="u-link"><i class="o-icon--large o-icon-download"/></span>
+			<svg viewBox="0 0 24 24">
+				<circle cx="12" cy="12" r="30"/>
+				<circle cx="12" cy="12" r="11"/>
+				<path d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+			</svg>
+		</div>
+		`)
+		btn.icon = btn.find('span').find('i.o-icon--large')
+		btn.progress = btn.find('span').find('svg.progress')
+
 		// CLick Action
-		Button.on('click', (e) => {
+		btn.on('click', (e) => {
 			e.preventDefault()
-			icon.removeClass('o-icon-download').addClass('o-icon-download-progress')
+			btn.icon.removeClass('o-icon-download').addClass('o-icon-download-progress')
 			// Get song Data
 			getSongsData('song', token, (result) => {
-				if (!result) return _done(icon, 'Song')
+				if (!result) return _done(btn.icon, 'Song')
 				toast(`Now Downloading Song : ${result.title}`)
 				downloadWithData(
 					result,
-					() => _done(icon),
-					(e) => _done(icon, e && 'Song')
+					() => _done(btn.icon),
+					(e) => _done(btn.icon, e && 'Song')
 				)
 			})
 		})
 		// Add Button 
-		$(this).parents('article.o-snippet').find('.o-snippet__item:nth-last-of-type(2)').before(Button)
+		$(this).parents('article.o-snippet').find('.o-snippet__item:nth-last-of-type(2)').before(btn)
 	})
 }
 // Playlist Download =====
@@ -65,26 +81,26 @@ const add_list_download_btn = () => {
 	firstBtn.after(Button)
 }
 // Add Download Quality selector on the Menu..
-const createDownloadQuality = () => {
-	var
-		menuItem = $('<aside id="quality-dropdown" class="c-dropdown u-margin-right@sm"><div class="c-dropdown__header"><span class="c-dropdown__type"><span class="u-visible-visually@lg"></span>Qiality</span> <span class="c-dropdown__select curr_biterate"></span></div></aside>'),
-		dropDown = $('<div class="c-dropdown__content"><div class="u-padding@sm"><h5 class="u-deci u-margin-bottom-none@sm">Download Quality</h5><p class="u-centi u-color-js-gray-alt-light u-margin-bottom-none@sm"><em>Pick a Preferred Quality</em></p></div><div class="o-message o-message--error">You must select a bitrate</div></div>'),
-		dropDownList = $('<form id="song-biterate"><section class="u-scroll u-3/5-max-vh"><ul class="o-list-select"></ul></section></form>')
-	// Biterates
-	let bitrates = ['320', '192', '128', '64', '32', '16'];
-	bitrates = bitrates.map((rate) => {
-		const el = $(`<li class='o-list-select__item'>${rate} kbps</li>`)
-		if (rate === localStorage.bitrate) {
+const add_download_quality = () => {
+	if ($('#quality-dropdown').length !== 0) return
+	let menuItem = $('<aside id="quality-dropdown" class="c-dropdown u-margin-right@sm"><div class="c-dropdown__header"><span class="c-dropdown__type"><span class="u-visible-visually@lg"></span>Qiality</span> <span class="c-dropdown__select curr_bitrate"/></div></aside>')
+	let dropDown = $('<div class="c-dropdown__content"><div class="u-padding@sm"><h5 class="u-deci u-margin-bottom-none@sm">Download Quality</h5><p class="u-centi u-color-js-gray-alt-light u-margin-bottom-none@sm"><em>Pick a Preferred Quality</em></p></div><div class="o-message o-message--error">You must select a bitrate</div></div>')
+	let dropDownList = $('<form id="song-bitrate"><section class="u-scroll u-3/5-max-vh"><ul class="o-list-select"></ul></section></form>')
+	// Bitrates
+	let bitrates = [[320, 'Extreme'], [160, 'Best'], [96, 'Good'], [48, 'Fair'], [12, 'Low']];
+	bitrates = bitrates.map(([rate, name]) => {
+		const el = $(`<li class='o-list-select__item'>${name}</li>`)
+		if (rate.toString() === localStorage.bitrate) {
 			el.addClass('selected')
-			menuItem.find('.curr_biterate').first().text(`${rate} kbps`)
+			menuItem.find('.curr_bitrate').text(name)
 		}
 		// Click Action
 		el.on('click', e => {
 			localStorage.bitrate = rate
 			$(e.target).parent().find('.selected').removeClass('selected')
 			$(e.target).addClass('selected')
-			menuItem.find('.curr_biterate').first().text(`${rate} kbps`)
-			toast(`Download Quality set to <strong>${rate} kbps</strong>`)
+			menuItem.find('.curr_bitrate').text(name)
+			toast(`Download Quality set to <strong>${name}</strong>`)
 		})
 		return el
 	})
@@ -108,7 +124,7 @@ const download_progress = () => {
 		$('.c-player').after(wrapper)
 }
 // Hide ads and promotions
-var hideAds = () => {
+var hide_ads = () => {
 	$('body').removeClass('promo')
 	$('.banner').removeClass('banner')
 	const ads = ['.ad', '.c-promo', '.c-ad', '.c-banner', '.c-player__ad']
@@ -134,17 +150,17 @@ const toast = (message) => {
 // Run on Plugin Initialization
 var initPlugin = () => {
 	console.clear()
-	hideAds()
+	hide_ads()
 	add_song_download_btn()
 	// add_album_download_btn()
 	add_list_download_btn()
+	add_download_quality()
 }
 // Ready state
 $(document).ready(() => {
 	localStorage.bitrate = localStorage.bitrate || 320
 	initPlugin()
 	download_progress()
-	createDownloadQuality()
 	// Create toast to notify
 	toast('Plugin is active and Functional')
 	// hide download bar if no current downloads
@@ -153,12 +169,10 @@ $(document).ready(() => {
 			$('#download-bar').removeClass('active').find('label').removeAttr('data-c')
 	}, 2000)
 	// check if classes of the .page-wrap changes then add the buttons again
-	var a = 0, b, p = window.location.href, q
+	let a = $('#root > *:last-child').attr('class')
 	// detect changes
 	setInterval(() => {
-		b = $('ol.o-list-bare').find('li').length || 0
-		q = window.location.href
-		if (b !== a) { initPlugin(); a = b }
-		if (p !== q) { initPlugin(); p = q }
+		let b = $('#root > *:last-child').attr('class')
+		if (b !== a) { initPlugin(); a = b; console.log(1) }
 	}, 1000)
 })
