efe4e598436961e393b93fbb6594a8e7f799b10b
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-02-08 14:44:04 +0530

    theme toggle functional

--- source/browser/background.ts
+++ source/browser/background.ts
@@ -1,14 +1,25 @@
 import browser from 'webextension-polyfill'
 
-// set default quality
-const { quality }: any = browser.storage.sync.get(['quality'])
-if (!quality) browser.storage.sync.set({ quality: '160' })
-
-interface RequestDetails extends browser.WebRequest.OnHeadersReceivedDetailsType {}
-
 console.log('Background Page running successfully')
 
+// set default quality on first run
+browser.runtime.onInstalled.addListener(() => {
+	browser.storage.sync.set({ quality: '160' })
+})
+
+// open jiosaavn on clicking the extension icon
+browser.browserAction.onClicked.addListener(() => {
+	browser.tabs.create({ url: 'https://www.jiosaavn.com/' })
+})
+
+// prettier-ignore
+const CSS = {
+	light: ':root{--primary:#2cd8c4;--bg-1:#f6f6f6;--bg-2:#eee;--bg-3:#bbb;--border:#e9e9e9;--text-1:#3e3e3e;--text-2:#555;--text-3:#888}',
+	dark: ':root{--primary:#2cd8c4;--bg-1:#18191a;--bg-2:#242526;--bg-3:#2e3031;--border:#292b2c;--text-1:#eee;--text-2:#888;--text-3:#555}',
+}
+
 const handlers: any = {}
+interface RequestDetails extends browser.WebRequest.OnHeadersReceivedDetailsType {}
 const cors = {
 	onHeaderReceived(details: RequestDetails) {
 		const host = details.url && new URL(details.url).hostname
@@ -27,7 +38,7 @@ const cors = {
 		// Modify response headers
 		browser.webRequest.onHeadersReceived.addListener(
 			newHandler,
-			{ urls: ['<all_urls>'], tabId: id },
+			{ urls: ['https://*.cdnsrv.jio.com/*', 'https://*.saavncdn.com/*'], tabId: id },
 			['blocking', 'responseHeaders']
 		)
 	},
@@ -43,7 +54,12 @@ browser.tabs.onUpdated.addListener((id, info) => {
 	if (info?.status == 'loading')
 		browser.tabs.get(id).then((tab) => {
 			const host = tab.url && new URL(tab.url).hostname
-			if (host?.includes('jiosaavn.com')) cors.install(id)
+			if (!host?.includes('jiosaavn.com')) return
+			cors.install(id)
+			browser.storage.sync.get('theme').then((i) => {
+				const theme: 'light' | 'dark' = i.theme || 'light'
+				browser.tabs.insertCSS({ runAt: 'document_start', code: CSS[theme] })
+			})
 		})
 })
 
